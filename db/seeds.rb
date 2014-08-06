42d9b5f8b1ed2fe3e60f0644bf7cb3ddd56f973a
Sarah Vessels <sarah.vessels@cirrusmio.com>
2014-08-06 19:12:34 -0400

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,20 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Rating.create!([
+  {dimension: 'chicken tacos', stars: 3.0, rater: 'Tanzi',
+   comment: 'His chicken was too spicy, but still tasty. I give him 3 stars.'},
+  {dimension: 'amenability', stars: 5.0, rater: 'Tanzi',
+   comment: 'So amenable. 5 stars.'},
+  {dimension: 'DotA', stars: 5.0, rater: 'Sarah',
+   comment: 'he was a real feeder in DotA tonight, 5/10'},
+  {dimension: 'diving', stars: 7.0, rater: 'Steve',
+   comment: 'we went swimming today and his dives were as follows: ' +
+            '7/10, 6/10, 7/10, 5.5/10'},
+  {dimension: 'diving', stars: 6.0, rater: 'Steve',
+   comment: 'we went swimming today and his dives were as follows: ' +
+            '7/10, 6/10, 7/10, 5.5/10'},
+  {dimension: 'diving', stars: 7.0, rater: 'Steve',
+   comment: 'we went swimming today and his dives were as follows: ' +
+            '7/10, 6/10, 7/10, 5.5/10'},
+  {dimension: 'diving', stars: 5.5, rater: 'Steve',
+   comment: 'we went swimming today and his dives were as follows: ' +
+            '7/10, 6/10, 7/10, 5.5/10'}
+])
