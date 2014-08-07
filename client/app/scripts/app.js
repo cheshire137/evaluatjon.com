c4f16989a847a27e6fde1d988a40693229fdfdaa
Sarah Vessels <sarah.vessels@cirrusmio.com>
2014-08-06 20:23:21 -0400

    Don't allow changing static ratings

--- client/app/scripts/app.js
+++ client/app/scripts/app.js
@@ -36,6 +36,40 @@ app.directive('starRating', function() {
   return {
     restrict: 'A',
     template: '<ul class="star-rating">' +
+              ' <li ng-repeat="star in stars" ng-class="star">' +
+              '  <span class="star"></span>' +
+              ' </li>' +
+              '</ul>',
+    scope: {
+      ratingValue: '=',
+      max: '=',
+      onRatingSelected: '&'
+    },
+    link: function(scope) {
+      var update_rating = function() {
+        scope.stars = [];
+        for (var i=0; i<scope.max; i++) {
+          scope.stars.push({
+            filled: i < scope.ratingValue,
+            half: i - 0.5 === scope.ratingValue
+          });
+        }
+      };
+      scope.$watch('ratingValue',
+        function(old_value, new_value) {
+          if (new_value) {
+            update_rating();
+          }
+        }
+      );
+    }
+  };
+});
+
+app.directive('starRater', function() {
+  return {
+    restrict: 'A',
+    template: '<ul class="star-rating mutable">' +
               ' <li ng-repeat="star in stars" ng-class="star" ng-click="toggle($index)">' +
               '  <span class="star"></span>' +
               ' </li>' +
