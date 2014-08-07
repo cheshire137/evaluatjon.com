e8208c8919ca6f12a7be47109a907dc4a5131be9
Sarah Vessels <sarah.vessels@cirrusmio.com>
2014-08-06 20:09:18 -0400

    Display whole stars for ratings

--- client/app/scripts/app.js
+++ client/app/scripts/app.js
@@ -14,15 +14,12 @@ var app = angular.module('evaluatjonApp', [
     'ngResource',
     'ngRoute',
     'ngSanitize',
-    'ngTouch'
+    'ngTouch',
+    'angularMoment'
   ]);
 
 app.config(function ($routeProvider) {
   $routeProvider.when('/', {
-    templateUrl: 'views/main.html',
-    controller: 'MainCtrl'
-  });
-  $routeProvider.when('/ratings', {
     templateUrl: 'views/ratings.html',
     controller: 'RatingsCtrl'
   });
@@ -34,3 +31,42 @@ app.factory('Rating', ['$resource', function ($resource) {
     'update': { method:'PUT' }
   });
 }]);
+
+app.directive('starRating', function() {
+  return {
+    restrict: 'A',
+    template: '<ul class="star-rating">' +
+              ' <li ng-repeat="star in stars" ng-class="star" ng-click="toggle($index)">' +
+              '  <span class="star"></span>' +
+              ' </li>' +
+              '</ul>',
+    scope: {
+      ratingValue: '=',
+      max: '=',
+      onRatingSelected: '&'
+    },
+    link: function(scope) {
+      var updateStars = function() {
+        scope.stars = [];
+        for (var i=0; i<scope.max; i++) {
+          scope.stars.push({
+            filled: i < scope.ratingValue
+          });
+        }
+      };
+      scope.toggle = function(index) {
+        scope.ratingValue = index + 1;
+        scope.onRatingSelected({
+          rating: index + 1
+        });
+      };
+      scope.$watch('ratingValue',
+        function(oldVal, newVal) {
+          if (newVal) {
+            updateStars();
+          }
+        }
+      );
+    }
+  };
+});
