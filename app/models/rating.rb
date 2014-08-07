6740ca71328295268ddc53fe8a3aea1869ab88e4
Sarah Vessels <sarah.vessels@cirrusmio.com>
2014-08-06 21:12:02 -0400

    Require rater

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,4 +1,5 @@
 class Rating < ActiveRecord::Base
   validates :dimension, presence: true
   validates :stars, presence: true, numericality: {greater_than_or_equal_to: 0}
+  validates :rater, presence: true
 end
