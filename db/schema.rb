be04ab96e47bebf13d955a61093cd936d36c9423
Sarah Vessels <cheshire137@gmail.com>
2017-08-09 23:47:31 -0500

    Drop trackable module from User model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140809014352) do
+ActiveRecord::Schema.define(version: 20170810044621) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,11 +39,6 @@ ActiveRecord::Schema.define(version: 20140809014352) do
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
     t.string   "auth_token"
     t.datetime "created_at"
     t.datetime "updated_at"
